5b8c44687016d471cfad84c6ad28f0594d4ba5b8
Muhamed <muhamedemad.eldin@outlook.com>
2020-05-18 23:44:03 +0300

    Found problem with name dir
    I found a problem when admin upload any content if name for this content contains (\/:*?<>"|)
    I found Error NotADirectoryError

--- sec_academy/books/utilise/utils_nav.py
+++ sec_academy/books/utilise/utils_nav.py
@@ -1,11 +1,8 @@
 import random
 import string
 from django.utils.text import slugify
-# '''
-# SLUG GENERATEOR AND random_string_generator is located here :
-# https://www.codingforentrepreneurs.com/blog/a-unique-slug-generator-for-django/ #SLUG FILE
-# http://joincfe.com/blog/random-string-generator-in-python/
-# '''
+
+
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
